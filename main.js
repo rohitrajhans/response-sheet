05a6434d836a798c8a4969b5791740c119c231bd
Rohit Rajhans <rohitrajhans07@gmail.com>
2020-05-18 22:18:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxyHF5gdcPzHtE3VkL-21nGfqa-Pt1p0TlgRhIG2wgZIT56fWjR/exec'
-  const form = document.forms['submit-to-sheet']
+const form = document.forms['submit-to-sheet']
 
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'GET', body: new FormData(form)})
-      .then(response => { alert('Successfully submitted'); location.reload(); })
-      .catch(error => console.error('Error!', error.message))
-  })
+//   form.addEventListener('submit', e => {
+//     e.preventDefault()
+//     fetch(scriptURL, { method: 'GET', body: new FormData(form)})
+//       .then(response => { alert('Successfully submitted'); location.reload(); })
+//       .catch(error => console.error('Error!', error.message))
+//   }) 
+
+$('#submit-form').on('submit', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: scriptUrl,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    console.log("Updated");
+  );
+})
