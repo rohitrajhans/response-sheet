6480a3fc116a318866bf115e08961ba0d47fa641
Rohit Rajhans <rohitrajhans07@gmail.com>
2020-05-18 22:32:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,13 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbxyHF5gdcPzHtE3VkL-21nGfqa-Pt1p0TlgRhIG2wgZIT56fWjR/exec'
-const form = $('#response-form");
+const form = $('#response-form');
 
 //   form.addEventListener('submit', e => {
 //     e.preventDefault()
-//     fetch(scriptURL, { method: 'GET', body: new FormData(form)})
+//     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
 //       .then(response => { alert('Successfully submitted'); location.reload(); })
 //       .catch(error => console.error('Error!', error.message))
 //   }) 
 
-// $('#submit-form').on('click', function(e) {
-//   e.preventDefault();
-//   var jqxhr = $.ajax({
-//     url: scriptURL,
-//     method: "GET",
-//     dataType: "json",
-//     data: form.serialize()
-//   }).success(
-//     // do something
-//   );
-// })
 
 form.submit(function (e) {
             $.ajax({
